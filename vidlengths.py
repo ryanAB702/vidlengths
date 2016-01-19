7e71e8830908a6c5abd27c3600deacb64dd573ce
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-01-19 09:32:37 -0500

    Fix #1
    Script handles .mov files now. In cases where there's bad input (e.g.
    non mp4 files masquerading as .mp4 files), we catch an IndexError
    exception and continue on to the next file.

--- vidlengths.py
+++ vidlengths.py
@@ -2,12 +2,18 @@ import sys
 import csv
 import subprocess
 import os
+import argparse
 
 from ffprobe import FFProbe
 
 start_dir = ""
 csv_path = ""
 output_table = []
+
+nomp4 = False
+nomov = False
+
+args = None
 
 def output_csv():
 
@@ -18,18 +24,39 @@ def output_csv():
 def walk_tree(path):
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.endswith(".mp4"):
+            if check_extension(file):
                 pathname = os.path.join(root, file)
                 metadata = FFProbe(pathname)
-                length = metadata.streams[0].durationSeconds()
+                try:
+                    length = metadata.streams[0].durationSeconds()
+                except (IndexError):
+                    print os.path.join(root, file) + " was a problem file. skipped."
+                    continue
                 output_table.append((pathname, length))
-                       
+
+def check_extension(filename):
+    if filename.endswith(".mp4"):
+        return False if nomp4 else True
+    if filename.endswith(".mov"):
+        return False if nomov else True
+    else:
+        return False
+
+def print_usage():
+    print "\nUSAGE:\n"
+    print "$: python vidlengths.py [directory] [output_csv] [-nomp4] [-nomov]\n"
+
 if __name__ == "__main__":
-    
+
+    print_usage()
+
     start_dir = sys.argv[1]
     csv_path = sys.argv[2]
-    
+
+    if "-nomp4" in sys.argv:
+        nomp4 = True
+    if "-nomov" in sys.argv:
+        nomov = True
+
     walk_tree(start_dir)
     output_csv()
-
-    
