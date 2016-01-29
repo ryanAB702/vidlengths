54b23435ffc4c62f3a86c281d2438880ba086d0b
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-01-29 10:11:49 -0500

    filter subject_files.csv

--- filter.py
+++ filter.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-def filter():
+def filter_prefix():
     with open("subject_files.csv", "rU") as input_file:
         with open("subject_files_filtered.csv", "wb") as output_file:
             reader = csv.reader(input_file)
@@ -18,7 +18,25 @@ def filter():
                 writer.writerow([joined_path, row[1]])
                 # path = os.path.split(row[0])
 
+def filter_name():
+    with open("subject_files.csv", "rU") as input_file:
+        with open("subject_files_filtered.csv", "wb") as output_file:
+            reader = csv.reader(input_file)
+            writer = csv.writer(output_file)
+
+            reader.next()
+            writer.writerow(["file", "length(s)"])
+            for row in reader:
+                path = row[0]
+                filename = os.path.split(path)[1]
+                if "_video.mp4" in filename:
+                    writer.writerow([path, row[1]])
+                else:
+                    continue
 
 if __name__ == "__main__":
 
-    filter()
+    # filter_prefix()
+    filter_name()
+
+
