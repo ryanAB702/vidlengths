9d74edd2a6ea366d4ef0066d3c8951c7243e84b1
Ryan Bloom <Ryan@Ryans-MacBook-Pro-4.local>
2017-01-13 14:32:36 -0500

    Vidlengths with added comments

--- vidlengths.py
+++ vidlengths.py
@@ -6,6 +6,7 @@ import argparse
 
 from ffprobe import FFProbe
 
+#Initialize variables start_dir; csv_path; output_table; nomp4; nomov; and args
 start_dir = ""
 csv_path = ""
 output_table = []
@@ -15,25 +16,33 @@ nomov = False
 
 args = None
 
+#Function creates and writes to an output file
 def output_csv():
 
     with open(csv_path, "wb") as file:
         writer = csv.writer(file)
         writer.writerows(output_table)
 
+
 def walk_tree(path):
+    #Loop through files in start_dir and generate file names
     for root, dirs, files in os.walk(path):
         for file in files:
+            #Call helper function check_extension to check if file types match (.mp4 or .mov)
             if check_extension(file):
                 pathname = os.path.join(root, file)
                 metadata = FFProbe(pathname)
+                #Time the length of the file in seconds
                 try:
                     length = metadata.streams[0].durationSeconds()
+                #Throw exception if error is encountered so that the code does not break
                 except (IndexError):
                     print os.path.join(root, file) + " was a problem file. skipped."
                     continue
+                #Append the times to a table corresponding to the pathname or the file being timed
                 output_table.append((pathname, length))
 
+#Helper function that checks if the file type (mp4 or mov) is suitable
 def check_extension(filename):
     if filename.endswith(".mp4"):
         return False if nomp4 else True
@@ -42,6 +51,7 @@ def check_extension(filename):
     else:
         return False
 
+#Adds print statements
 def print_usage():
     print "\nUSAGE:\n"
     print "$: python vidlengths.py directory output_csv [-nomp4] [-nomov]\n"
@@ -53,6 +63,7 @@ if __name__ == "__main__":
     start_dir = sys.argv[1]
     csv_path = sys.argv[2]
 
+    #Set variable nomp4 and nomov to True or False based on the presense of "-nomp4" or "-nomov" in the argument
     if "-nomp4" in sys.argv:
         nomp4 = True
     if "-nomov" in sys.argv:
